6960369647a02628c2dfe1fa154c239a1131c25c
ZeroIntensity <zintensitydev@gmail.com>
2023-11-27 21:54:54 -0500

    dont mind me just some gen stuff

--- gen.py
+++ gen.py
@@ -1,78 +1,139 @@
 from __future__ import annotations
 
 import asyncio
-from typing import Any, NamedTuple
+import json
+from dataclasses import dataclass
+from typing import Any
 
 import aiohttp
 from bs4 import BeautifulSoup
 
 
-class Source(NamedTuple):
+@dataclass()
+class Source:
     attributes: dict[str, str]
     types: list[str]
     elements: list[str]
     components: list[str]
 
 
+NEWLINE: str = "\n"
+
+
+def indent(line: str, amount: int = 1) -> str:
+    return line + ("    " * amount)
+
+
 async def _add_elements(
     url: str,
     source: Source,
     session: aiohttp.ClientSession,
-    name: str,
+    ele_name: str,
 ) -> None:
-    print(f"Downloading {name}...")
-    attr_src: list[str] = []
+    try:
+        print(f"Downloading {ele_name}...")
+        attr_src: list[str] = []
+        attr_list = []
+        param_src = []
+        param_set_src = []
 
-    async with session.get(url) as res:
-        text = await res.text()
-        soup = BeautifulSoup(text)
-        attrs = soup.find(id="attributes")
-        assert attrs
-        parent = attrs.parent
-        assert parent
-        div = parent.find("div")
-        assert div
-        dl = div.find("dl")
-        assert dl
+        async with session.get(url) as res:
+            text = await res.text()
+            soup = BeautifulSoup(text, features="html.parser")
+            attrs = soup.find(id="attributes")
+            assert attrs
+            parent = attrs.parent
+            assert parent
+            div = parent.find("div")
+            assert div
+            dl = div.find("dl")
+            if dl:
+                print(f"Handling attributes for {url}")
+                for i in dl.find_all("dt"):  # type: ignore
+                    if i.parent != dl:
+                        continue
 
-        print(f"Handling attributes for {url}")
-        for i in dl.find_all("dt"):  # type: ignore
-            print(i.get("id"))
+                    if i.find("abbr"):
+                        continue
 
-    src = f"""class {name.capitalize()}(Element):
-    TAG_NAME: ClassVar[str] = "{name}"
-    ATTRIBUTE_LIST: ClassVar[list[str]] = [{attr_list}]
+                    name = i.get("id")
+                    attr_list.append(name)
+                    tp = source.attributes.get(name)
+                    if not tp:
+                        tp = input(f"Type for {name}: ")
+                        source.attributes[name] = tp
 
-    def __init__(self, *content: Content, attributes: GlobalAttributes) -> None:
+                        with open("attributes.json", "w") as f:
+                            json.dump(source.attributes, f)
+
+                    tp_name = f"{name.capitalize()}Type"
+                    source.types.append(f"{tp_name} = {tp}")
+                    if "present" not in tp.lower():
+                        attr_src.append(f"self.{name}: {tp_name} = {name}")
+                    else:
+                        attr_src.append(
+                            f"self.{name}: {tp_name} = {name} if {name} is not True else ''"
+                        )
+
+                    param_src.append(f"{name}: {tp_name} | None = None,")
+                    param_set_src.append(f"{name}={name}")
+
+        src = f"""class {ele_name.capitalize()}(Element):
+    TAG_NAME: ClassVar[str] = "{ele_name}"
+    ATTRIBUTE_LIST: ClassVar[list[str]] = {attr_list}
+
+    def __init__(
+        self,
+        *content: Content,
+        attributes: GlobalAttributes,
+        {indent(NEWLINE, 2).join(param_src)}
+    ) -> None:
         super().__init__(content, attributes)
-        {attr_src}
-    """
-    source.elements.append(src)
+        {indent(NEWLINE, 2).join(attr_src)}
+        """
+        source.elements.append(src)
+        source.components.append(
+            f"""def {ele_name}(
+    *content: Content,
+    {indent(NEWLINE).join(param_src)}
+    **global_attributes: GlobalAttributes,
+) -> {ele_name.capitalize()}:
+    return {ele_name.capitalize()}(
+        *content,
+        global_attributes,
+        {indent(NEWLINE, 2).join(param_set_src)}
+    )"""
+        )
+    except KeyboardInterrupt:
+        print(
+            "\n".join(source.types),
+            "\n".join(source.elements),
+            "\n".join(source.components),
+            sep="\n",
+        )
+        exit()
 
 
 async def _search_elements(
     element: Any, source: Source, session: aiohttp.ClientSession
 ):
-    tasks = []
     for i in element.children:
         a = i.find("a")
         if not i.find("abbr"):
             code = a.find("code")
             text = code.get_text().replace("<", "").replace(">", "")
-            tasks.append(
-                _add_elements(
-                    "https://developer.mozilla.org" + a.get("href"),
-                    source,
-                    session,
-                    text,
-                )
+            await _add_elements(
+                "https://developer.mozilla.org" + a.get("href"),
+                source,
+                session,
+                text,
             )
-
-    await asyncio.gather(*tasks)
 
 
 async def main():
     source = Source(attributes={}, types=[], elements=[], components=[])
+    with open("attributes.json") as f:
+        source.attributes = json.load(f)
     async with aiohttp.ClientSession() as session:
         async with session.get(
             "https://developer.mozilla.org/en-US/docs/Web/HTML/Element"
